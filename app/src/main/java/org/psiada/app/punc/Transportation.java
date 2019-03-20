c8c5032f9b7f5dc442f9516de7e9011d1398d02e
Mihir Alve <mihir@Meatball.local>
2019-03-19 23:29:38 -0400

    i forget what I did earlier rip

--- app/src/main/java/org/psiada/app/punc/Transportation.java
+++ app/src/main/java/org/psiada/app/punc/Transportation.java
@@ -220,7 +220,7 @@ public class Transportation extends Fragment implements OnMapReadyCallback, Goog
 
     @Override
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(getActivity(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(getActivity(), "MyLocation button clicked", Toast.LENGTH_SHORT).show();
         // Return false so that we don't consume the event and the default behavior still occurs
         // (the camera animates to the user's current position).
         return false;
